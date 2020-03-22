d455f8f4725ad63a26080894fc7877cf276b1cee
unknown <mehedi5051@gmail.com>
2020-03-22 21:35:53 +0600

    by using useQuery hook, querying data

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,10 +1,29 @@
 import React from "react";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+
+const getBookQuery = gql`
+	{
+		books {
+			name
+			id
+		}
+	}
+`;
 
 const BookList = () => {
+	const { loading, error, data } = useQuery(getBookQuery, {});
+
+	if (loading) return <h4>Loading...</h4>;
+
+	if (error) return <h5>{error}</h5>;
+
 	return (
 		<div>
 			<ul id="book-list">
-				<li>Book Name</li>
+				{data.books.map(book => (
+					<li key={book.id}>{book.name}</li>
+				))}
 			</ul>
 		</div>
 	);
--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+
+// Allow Cross Origin Requests
+app.use(cors());
 
 mongoose.connect(
 	"mongodb+srv://mehedi:mehedi@mehedi-mw5jy.mongodb.net/test?retryWrites=true&w=majority",
