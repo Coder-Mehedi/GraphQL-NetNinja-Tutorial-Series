5137e55f9344182b49cdea461fc020561760bbd3
unknown <mehedi5051@gmail.com>
2020-03-22 18:44:08 +0600

    simple mongod database setup

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose.connect(
+	"mongodb+srv://mehedi:mehedi@mehedi-mw5jy.mongodb.net/test?retryWrites=true&w=majority",
+	{ useUnifiedTopology: true, useNewUrlParser: true },
+	() => console.log("MongoDB Database Connected")
+);
 
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
